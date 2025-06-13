d285e2334eb34f4db0f9fd4ab13b60cff0f965b3
Roshni <Roshni@LAPTOP-H1B61DPF>
2025-06-13 22:48:08 +0530

    Updated entity annotations and relationships.

--- Category_Product_task/src/main/java/in/product/entities/Category.java
+++ Category_Product_task/src/main/java/in/product/entities/Category.java
@@ -2,6 +2,9 @@ package in.product.entities;
 
 import java.util.List;
 
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -9,11 +12,15 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name="CATEGORY_TBL")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
 
 	@Id
@@ -22,5 +29,6 @@ public class Category {
 	private String name;
 	private String description;
 	@OneToMany(mappedBy = "category",cascade=CascadeType.ALL)
+	@JsonManagedReference
 	private List<Product> products;
 }
--- Category_Product_task/src/main/java/in/product/entities/Product.java
+++ Category_Product_task/src/main/java/in/product/entities/Product.java
@@ -5,6 +5,8 @@ import java.time.LocalDateTime;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -13,12 +15,15 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name="PRODUCTS_TBL")
 @Data
-
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product {
 
 	@Id
@@ -38,5 +43,6 @@ public class Product {
     
 	@ManyToOne
 	@JoinColumn(name="category_id",nullable = false)
+	@JsonBackReference
 	private Category category;
 }
